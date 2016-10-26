0fcb17087041e0953b1222feed756bba7e76f2bd
Junior Zapata <jrszapata@hotmail.com>
2016-10-26 10:58:34 -0400

    Add label to detect the hostname at the top of the app.

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var app = express();
+var handlebars = require('handlebars');
+var fs = require('fs');
+var os = require('os');
+
+
+app.get('/', function(req, res) {
+
+  var tmpl = fs.readFileSync('src/index.handlebars', 'utf-8');
+  var compiled = handlebars.compile(tmpl)
+  var html = compiled({
+    'hostname': os.hostname()
+  });
+  
+  res.set('Content-type', 'text/html');
+  res.status(200);
+  res.send(html);
+  // res.sendFile(__dirname + '/public/index.html');
+});
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-  res.set('Content-type', 'text/html');
-  res.status(200);
-  res.sendFile(__dirname + '/public/index.html');
-});
-
 app.listen(80, function() {
   console.log('Listening at port 80');
-})
+});
